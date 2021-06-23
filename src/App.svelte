da4ba173f4388c94c9d11ec8717b7e24f9d7900b
maxgraze <maxene.graze@gmail.com>
2021-06-23 11:49:36 -0700

    set up scripts in a grid and hard-coded

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,89 @@
 <script>
-	import Scroller from './Utils/Scroller.svelte'
+	import Scroller from "./Utils/Scroller.svelte";
+	import Grid from "./Components/Grid.svelte";
 
 	export let scroller;
 	let index, offset, progress;
-
+	//education: tifingah, manchu, rajna,
+	//print western-cham, mandaic
+	//broadcast TV: CAS, ol chiki, yi
+	//hover over each to learn about what we do know about them
 	const steps = [
-		{ 
-			disappear1: true, 
-			disappear2: false,
-		}
-	]
+		{
+			education: false,
+			print: false,
+			broadcast: false,
+			signage: false,
+		},
+		{
+			signage: true,
+			education: false,
+			print: false,
+			broadcast: false,
+		},
+		{
+			signage: true,
+			education: true,
+			print: false,
+			broadcast: false,
+		},
+		{
+			signage: true,
+			education: true,
+			print: true,
+			broadcast: false,
+		},
+		{
+			signage: true,
+			education: true,
+			print: true,
+			broadcast: true,
+		},
+	];
 
-	$: props = steps[index]
+	$: props = steps[index];
 </script>
 
 <div class="wrapper">
-	<header></header>
-	<Scroller top={0.2} bottom={0.8} bind:this={scroller} bind:index bind:offset bind:progress>
-        <div class="background" slot="background">
-            <p>
-                This is the background content. It will stay fixed in place while the foreground
-                scrolls over the top.
-    
-            </p>
-            <p>Section {index + 1} is currently active.</p>
-        </div>
-        <div class="foreground" slot="foreground">
-
-            {#each steps as step, i}
-                <section>
-                    {i}
-                </section>
-            {/each}
-        </div>
-    </Scroller>
+	<header />
+	<Scroller
+		top={0.2}
+		bottom={0.8}
+		bind:this={scroller}
+		bind:index
+		bind:offset
+		bind:progress
+	>
+		<div class="background" slot="background">
+			<Grid {...props} />
+			<p>
+				This is the background content. It will stay fixed in place while the
+				foreground scrolls over the top.
+			</p>
+			<p>Section {index + 1} is currently active.</p>
+		</div>
+		<div class="foreground" slot="foreground">
+			{#each steps as step, i}
+				<section>
+					{i}
+				</section>
+			{/each}
+		</div>
+	</Scroller>
 </div>
 
 <style>
-    section {
-        height: 80vh;
-    }
-    .background,
-    .foreground {
-        text-align: center;
-    }
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        font-size: 4em;
-        font-weight: 100;
-    }
+	section {
+		height: 80vh;
+	}
+	.background,
+	.foreground {
+		text-align: center;
+	}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
 </style>
---
+++ src/Components/Grid.svelte
@@ -0,0 +1,125 @@
+<script>
+	import Scripts from "../Scripts/index.svelte";
+	import { fade } from "svelte/transition";
+
+	/*
+    TODO:
+    - grid should be a square and not a rectangle
+	- svgs should be centered
+    - gird should fill the whole screen if on a phone
+    - disappearing function and transition
+
+    FIXME: 
+    - put all into arrays and then delete? 
+    */
+
+	// export let education = false;
+	export let signage, education, print, broadcast;
+
+	$: console.log(education);
+
+	let scripts = [
+		{ name: "CAS", cat: "broadcast" },
+		{ name: "manchu", cat: "education" },
+		{ name: "ol-chiki", cat: "broadcast" },
+		{ name: "ranjana", cat: "education" },
+		{ name: "shuishu", cat: "signage" },
+		{ name: "tifinagh", cat: "education" },
+		{ name: "mandaic", cat: "print" },
+		{ name: "western-cham", cat: "broadcast" },
+		{ name: "yi", cat: "broadcast" },
+	];
+	// const index = scripts.index(item);
+	// if (~index) scripts["blank"];
+</script>
+
+<!-- <div class="wrapper"> -->
+<!-- <img src={scripts.yi} alt="yi script" /> -->
+<!-- <label>
+	<input type="checkbox" bind:checked={education} />
+</label> -->
+<div class="grid">
+	{#each scripts as script}
+		{#if !education && script.cat === "education"}
+			<div transition:fade|local class="box">
+				<Scripts name={script.name} />
+			</div>
+		{:else if education && script.cat === "education"}
+			<div class="box">
+				<Scripts name="blank" />
+			</div>
+		{/if}
+		{#if !print && script.cat === "print"}
+			<div class="box">
+				<Scripts name={script.name} />
+			</div>
+		{:else if print && script.cat === "print"}
+			<div class="box">
+				<Scripts name="blank" />
+			</div>
+		{/if}
+		{#if !signage && script.cat === "signage"}
+			<div class="box">
+				<Scripts name={script.name} />
+			</div>
+		{:else if signage && script.cat === "signage"}
+			<div class="box">
+				<Scripts name="blank" />
+			</div>
+		{/if}
+		{#if !broadcast && script.cat === "broadcast"}
+			<div class="box">
+				<Scripts name={script.name} />
+			</div>
+		{:else if broadcast && script.cat === "broadcast"}
+			<div class="box">
+				<Scripts name="blank" />
+			</div>
+		{/if}
+	{/each}
+	<!-- <div class="broadcast">
+		<Scripts name="CAS" />
+	</div>
+	<div class="education">
+		<Scripts name="manchu" />
+	</div>
+	<div class="print">
+		<Scripts name="mandaic" />
+	</div>
+	<div class="broadcast">
+		<Scripts name="ol-chiki" />
+	</div>
+	<div class="education">
+		<Scripts name="ranja" />
+	</div>
+	<div class="signage">
+		<Scripts name="shuishu" />
+	</div>
+	<div class="education">
+		<Scripts name="tifinagh" />
+	</div>
+	<div class="print">
+		<Scripts name="western-cham" />
+	</div>
+	<div class="broadcast">
+		<Scripts name="yi" />
+	</div> -->
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		display: inline-grid;
+		/* grid-template-columns: auto auto auto; */
+		grid-template-columns: repeat(3, minmax(0, 1fr));
+		place-items: center;
+		/* margin: 5% 20% 20% 20%; */
+		margin: auto;
+		aspect-ratio: 1;
+		width: 40%;
+	}
+	/* this doesn't work */
+	.grid .box {
+		border: 1px dashed rgb(100, 100, 100);
+	}
+</style>
